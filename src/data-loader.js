a77218c1711761a05bde9ff040a3770d93656278
huyen-nguyen <22818938+huyen-nguyen@users.noreply.github.com>
2022-08-01 02:38:16 -0500

    Read in csv and tsv OK

--- src/data-loader.js
+++ src/data-loader.js
@@ -8,70 +8,108 @@ A. Logic to process input type:
 
  B. Format
  |----------|   Time    |   Text    |----------|
- 
+
  C. Signatures
  csv: text/csv
  tsv: text/tab-separated-values
  txt: text/plain
  xls: application/vnd.ms-excel
  xlsx: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
- 
+
  PIPELINE:
  - Load data
  - Read data
  - Show the data table
- 
+
  */
 // input file specifications
-const fileType = ['csv', 'tsv', 'txt', 'xls', 'xlsx']	// file types
 const delimiter = [] // (for txt file)
 
 // load local file
-const filepicker = document.getElementById("filepicker");
+const filepicker = document.getElementById("uploadInput");
 const output = document.getElementById('output');
+// const outputTextContent = document.getElementById('fileContent');
 filepicker.addEventListener("change", handleFiles);
 
 function handleFiles(event) {
 	const files = event.target.files;
+
+	const file = files[0];
+	const signature = file.type;
+	let data, type;
+
 	output.textContent = '';
+	// output.textContent += `File name: ${file.name}: ${file.type || 'unknown'}\n`;
+	output.textContent += `File name: ${file.name}, size: ${updateSize(files) || 'unknown'}\n`;
 
-	for (const file of files) {
-		output.textContent += `${file.name}: ${file.type || 'unknown'}\n`;
-		// TODO: (2a): detect file type, assume the extension is correct.
-		//  if not corerct, then try to use all other different parsers.
-
-	}
 	// ---------
-
-	const fileList = this.files; /* now you can work with the file list */
-	var file = fileList[0];
 	var reader = new FileReader();
 
-	// TODO: (1) store raw user input in `data`
 	reader.onload = function (e) {
-		let loading = d3.select('body').append('div').attr('id', 'loading');
-		// loading.append('img')
-		// 	.attr('id', 'loading-image')
-		// 	.attr('src', 'images/squareloader2.gif')
-		// 	.attr("height", "200")
-		// 	.attr("height", "200");
-
-		console.log(e.target.result);
-		// debugger
+		// TODO: (1) store raw user input in `data`
+		const rawData = e.target.result;
+		console.log(rawData);
 
 		// TODO: (2b-1) reading it
-		let data = d3.csvParse(e.target.result).map(d => d);
+		// find file type based on signature
+		let typeIndex = Object.keys(fileSignature).indexOf(signature);
 
-		console.log(data)
+		if (typeIndex >= 0){
+			type = fileSignature[signature];
+			console.log(type)
+			data = window[type + 'Read'](rawData);
+			console.log(data);
+		}
+		else {
+			console.log("Wrong input type!")
+		}
+
+		// show data on screen
+
+
+
+		// ask user whether want to use this parsing of data or other specification
 	};
 	reader.readAsText(file);
 
-	console.log(reader);
-	console.log(file);
+}
 
-	// TODO: (2b) Try to read input file using different parsers. List functions here.
-	
-	function csvRead(e){
-		return d3.csvParse(e.target.result).map(d => d);
+// TODO: (2b) Try to read input file using different parsers. List functions here.
+
+function csvRead(rawData){
+	return d3.csvParse(rawData).map(d => {
+		return {
+			Time: d['Time'],
+			Text: d['Text'],
+		}
+	});
+}
+function tsvRead(rawData){
+	return d3.tsvParse(rawData).map(d => {
+		return {
+			Time: d['Time'],
+			Text: d['Text'],
+		}
+	});
+}
+
+function updateSize(files) {
+	let nBytes = 0,
+		oFiles = files,
+		nFiles = oFiles.length;
+	for (let nFileId = 0; nFileId < nFiles; nFileId++) {
+		nBytes += oFiles[nFileId].size;
 	}
+	let sOutput = nBytes + " bytes";
+	// optional code for multiples approximation
+	const aMultiples = ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+	for (let nMultiple = 0, nApprox = nBytes / 1024; nApprox > 1; nApprox /= 1024, nMultiple++) {
+		sOutput = nApprox.toFixed(3) + " " + aMultiples[nMultiple] + " (" + nBytes + " bytes)";
+	}
+	// end of optional code
+	return sOutput;
+}
+
+function renderTabularData(){
+
 }
