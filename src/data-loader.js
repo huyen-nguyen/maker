719f9b7d0a69ad339c4519d205e38fa554c7ab54
huyen-nguyen <22818938+huyen-nguyen@users.noreply.github.com>
2022-08-01 23:42:26 -0500

    Done stage 1: Load data and related check

--- src/data-loader.js
+++ src/data-loader.js
@@ -24,7 +24,7 @@ A. Logic to process input type:
  */
 // input file specifications
 const delimiter = [] // (for txt file)
-let fileLoadedFlag = false;
+let fileLoadedFlag = false, fieldFlag = false;
 
 // ------- button to choose file, instead of the default -------
 const fileSelect = document.getElementById("fileSelect"),
@@ -42,30 +42,37 @@ const output = document.getElementById('output');
 const visTrigger = document.getElementById('vis-trigger');
 const alertFile = document.getElementById('alert-file');
 const previewData = document.getElementById('preview-data');
-const loading = document.getElementById('loading')
+const loading = d3.select("#loading");
+const alertField = d3.select("#alert-field");
 
 filepicker.addEventListener("change", handleFiles);
-filepicker.addEventListener("click", () => {
-});
+
 visTrigger.addEventListener("click", () => {
-	if (fileLoadedFlag){
-		visualize()
+	if (!fileLoadedFlag){
+		alertFile.innerHTML = '<span class="text-warning">Select a file first!</span>';
+		visTrigger.setAttribute("href", "#");
+		return
+	}
+	else if (!fieldFlag){
+		visTrigger.setAttribute("href", "#");
+		return
 	}
 	else {
-		alertFile.innerHTML = "Select a file first!";
+		visualize();
+		visTrigger.setAttribute("href", "#features");
 	}
 })
 
 function handleFiles(event) {
-	d3.select("#loading").style("display", "inline-block")
-
+	loading.style("display", "inline-block")
+	alertField.style("display", "none")
 	const files = event.target.files;
 	const file = files[0];
 	const signature = file.type;
 	let rawDataForRender, type;
 
-	alertFile.textContent = '';
-	alertFile.textContent += `File name: ${file.name} | size: ${updateSize(files) || 'unknown'}\n`;
+	alertFile.innerHTML = '';
+	alertFile.innerHTML += 'File name: ' + file.name + '<br/>' + 'Size: ' + (updateSize(files)? updateSize(files) : 'unknown');
 
 	d3.select("#jsonTable").remove();
 
@@ -75,8 +82,7 @@ function handleFiles(event) {
 	reader.onload = function (e) {
 		// *store* raw user input in `data`
 		const rawData = e.target.result;
-		console.log(rawData);
-		fileLoadedFlag = true;
+		fileLoadedFlag = true;  // load successfully
 
 		// *read it*
 		// find file type based on signature
@@ -92,7 +98,7 @@ function handleFiles(event) {
 
 		// render preview
 		createTable(rawDataForRender);
-		console.log(rawDataForRender);
+		checkInputFields(rawDataForRender);
 
 		// ask user whether want to use this parsing of data or other specification
 	};
@@ -142,23 +148,22 @@ function updateSize(files) {
 	return sOutput;
 }
 
-function renderTabularData(){
-
-}
-
 function visualize(){
 
 }
 
-function createTable(object){
-	d3.select("#loading").style("display", "none")
-	
-	$('#preview-data').append('<table id="jsonTable" class="table"><thead><tr></tr></thead><tbody></tbody></table>');
 
-	$.each(Object.keys(object[0]), function(index, key){
+function createTable(rawDataForRender){
+	loading.style("display", "none")
+
+	$('#preview-data')
+		.append('<table id="jsonTable" class="table table-striped"><thead' +
+		' class="thead-light"><tr></tr></thead><tbody></tbody></table>');
+
+	$.each(Object.keys(rawDataForRender[0]), function(index, key){
 		$('#jsonTable thead tr').append('<th>' + key + '</th>');
 	});
-	$.each(object, function(index, jsonObject){
+	$.each(rawDataForRender, function(index, jsonObject){
 		if(Object.keys(jsonObject).length > 0){
 			var tableRow = '<tr>';
 			$.each(Object.keys(jsonObject), function(i, key){
@@ -168,4 +173,20 @@ function createTable(object){
 			$('#jsonTable tbody').append(tableRow);
 		}
 	});
+
+}
+
+function checkInputFields(rawDataForRender){
+	const fields = Object.keys(rawDataForRender[0]);
+	// case-sensitive
+	if (!fields.includes("Time") || !fields.includes("Text")){
+		fieldFlag = false;
+		alertField
+			.style("display", "block")
+			.html('<span>Missing columns named <code>Time</code> and/or' +
+			' <code>Text</code>! (case-sensitive)</span>')
+	}
+	else {
+		fieldFlag = true;
+	}
 }
