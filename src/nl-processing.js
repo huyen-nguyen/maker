6b07fd3497b0da57c7789cc294b5f0f55e435606
huyen-nguyen <22818938+huyen-nguyen@users.noreply.github.com>
2022-08-06 20:53:13 -0500

    TF-IDF

--- src/nl-processing.js
+++ src/nl-processing.js
@@ -48,16 +48,16 @@ function textProcessing(dataForNLP){
 				)
 				.sort((a,b) => b.values.length - a.values.length)
 				.splice(0, topWords)
-				.map(d => {return {text: d.key, frequency: d.values.length, topic: category, id: d.key + '_' + category + '_' + index}})
+				.map(d => {return {
+					text: d.key,
+					frequency: d.values.length,
+					tfidf: d.values[0].terms[0].tfidf*10,
+					topic: category,
+					id: d.key + '_' + category + '_' + index}})
 		})
 
 		dataForVis.push(obj);
-
 	})
-
-	if (repType === 'sudden'){
-		getSuddenData(dataForVis)
-	}
 
 	return dataForVis;
 }
